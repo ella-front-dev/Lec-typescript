fd256ceb8439469d322fb348628c6540f3d25033
EllaDev <ellawork23@gmail.com>
2022-03-23 00:50:19 +0900

    기본 예시- 변수

---
+++ basic_example/01_variable/1_type_basic.ts
@@ -0,0 +1,28 @@
+
+//TS - 배열
+let str1: string = 'hello';
+
+// TS - 숫자 
+let num: number = 100;
+
+// TS - 배열
+let arr: Array<number> = [1,2,3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 100];
+let test: number[] = [2,3,4];
+
+//  TS: 튜플 - 배열의 모든 인덱스의 타입을 지정 
+let address: [string, string, number] = ['gangnam', 'pangyo', 1000];
+
+// TS: 객체
+let obj: object = {};
+let person: object = {
+  name: 'bs',
+  age: 12
+}
+let person1: {name: string, age: number} ={
+  name: 'thor',
+  age: 1000
+}
+
+//  TS 진위값
+let show: boolean = true;
---
+++ basic_example/01_variable/sample_function.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+  return a + b;
+}
+sum(10, 20);
+
+// 인수 제한 없음
+sum(1,2,3,4)
